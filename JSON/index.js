688798a70bae9beea1d1fa655b971c2b6f210ebf
Ghansham29 <ghanshamsutar29@gmail.com>
2023-12-14 14:46:36 +0530

    added

--- JSON/index.js
+++ JSON/index.js
@@ -1,5 +1,26 @@
-// JSON : JavaScript Object Notation
-// Used : JSON is a text format for storing and transporting data
-// JSON is a lightweight data interchanging format
-// JSON is used to send data between computers
-// JSON is language independent (any language)
+// // JSON : JavaScript Object Notation
+// // Used : JSON is a text format for storing and transporting data
+// // JSON is a lightweight data interchanging format
+// // JSON is used to send data between computers
+// // JSON is language independent (any language)
+
+// var a = 1;
+// switch (a) {
+//     case 1:
+//         a += 'one';
+//     case 2:
+//         a += 'two';
+//     default:
+//         a+='three';
+// }
+// console.log(a);
+
+
+var a = 3;
+for (let i = 0; i < a; i++) {
+    if (i % 2 == 0) {
+
+        continue;
+    }
+    console.log(i);
+}
